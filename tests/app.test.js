53d53a998d9cb5d268d709e41667a2fca5b2518d
Nate Rana <n8.rana@gmail.com>
2022-04-16 03:11:07 -0500

    updated DB for local testing

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,25 @@
+const { Pool, Client} = require('pg');
+const db = require('../database/pg.js');
+
 function sum (a, b) {
   return a + b;
 }
 
+function checkPersistentDB() {
+  return db.getInfo('s@s.com', (result) => {
+    console.log(result);
+    return result;
+  }).catch((err)=> {
+    console.log(err);
+  })
+};
+
+
 test('add two numbers', () => {
   expect(sum(8, 9)).toBe(17);
 })
+
+
+// test('db connection', () => {
+//   expect(checkPersistentDB()).toBe(true);
+// })
